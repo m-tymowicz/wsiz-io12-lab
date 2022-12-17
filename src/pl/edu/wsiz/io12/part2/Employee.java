0a353870aebdf2fb5e4e7fc6a275bd88a43dc9ec
mariusztymowicz <mariusz.tymowicz@gmail.com>
2022-12-17 09:05:52 +0100

    2.4 done

--- src/pl/edu/wsiz/io12/part2/Employee.java
+++ src/pl/edu/wsiz/io12/part2/Employee.java
@@ -3,27 +3,32 @@ package pl.edu.wsiz.io12.part2;
 import java.util.Scanner;
 
 public class Employee {
-    String firstname;
-    String lastname;
-    int age;
+    private final String firstname;
+    private final String lastname;
+    private final int age;
 
     void print() {
         System.out.print(firstname + " " + lastname + " " + age);
     }
 
+    public Employee(String firstname, String lastname, int age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
     static Employee read() {
         Scanner scanner = new Scanner(System.in);
-        Employee e = new Employee();
 
         System.out.print("Podaj imię:      ");
-        e.firstname = scanner.next();
+        String tmpFirstname = scanner.next();
 
         System.out.print("Podaj nazwisko:  ");
-        e.lastname = scanner.next();
+        String tmpLastname = scanner.next();
 
         System.out.print("Podaj wiek:      ");
-        e.age = scanner.nextInt();
+        int tmpAge = scanner.nextInt();
 
-        return e;
+        return new Employee(tmpFirstname, tmpLastname, tmpAge);
     }
 }
--- src/pl/edu/wsiz/io12/part2/Program.java
+++ src/pl/edu/wsiz/io12/part2/Program.java
@@ -2,10 +2,12 @@ package pl.edu.wsiz.io12.part2;
 
 public class Program {
     public static void main(String[] args) {
-        Employee employee = Employee.read();
-
+        Employee employee = new Employee("Jan","Kowalski",31);
         employee.print();
+
+        System.out.println();
+
+        Employee employee2 = Employee.read();
+        employee2.print();
     }
-
-
 }
