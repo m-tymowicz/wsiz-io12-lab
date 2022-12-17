45b418c8b79c51fb06ee81ebfb7102a5f1345c9b
mariusztymowicz <mariusz.tymowicz@gmail.com>
2022-12-17 10:26:41 +0100

    2.5 done

--- src/pl/edu/wsiz/io12/part2/Program.java
+++ src/pl/edu/wsiz/io12/part2/Program.java
@@ -2,12 +2,8 @@ package pl.edu.wsiz.io12.part2;
 
 public class Program {
     public static void main(String[] args) {
-        Employee employee = new Employee("Jan","Kowalski",31);
-        employee.print();
+        Company company = new Company();
 
-        System.out.println();
-
-        Employee employee2 = Employee.read();
-        employee2.print();
+        company.add(new Employee("Jan", "Kowalski", 31));
     }
 }
