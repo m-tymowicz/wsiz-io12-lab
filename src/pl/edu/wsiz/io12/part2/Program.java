c347d41950a54d2d25b5f98b0cb3d40b45898eb6
mariusztymowicz <mariusz.tymowicz@gmail.com>
2022-12-18 11:48:22 +0100

    2.10 (switch)

--- src/pl/edu/wsiz/io12/part2/Program.java
+++ src/pl/edu/wsiz/io12/part2/Program.java
@@ -3,6 +3,9 @@ package pl.edu.wsiz.io12.part2;
 import java.util.Scanner;
 
 public class Program {
+
+    private static final int EXIT_OPERATION_CODE = 99;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Company company = new Company();
@@ -11,7 +14,9 @@ public class Program {
         company.add(new Employee("Anna", "Kowalczyk", 27));
         company.add(new Employee("Piotr", "Nowak", 36));
 
-        while (true) {
+        int operationNumber = 0;
+
+        while (operationNumber != EXIT_OPERATION_CODE) {
             System.out.println("Lista operacji:\n");
 
             System.out.println("1 - wypisz listę pracowników");
@@ -20,24 +25,28 @@ public class Program {
             System.out.println("9 - zakończ program\n");
 
             System.out.print("Podaj numer operacji: ");
-            int operationNumber = scanner.nextInt();
+            operationNumber = scanner.nextInt();
 
-            if (operationNumber == 1) {
-                company.printEmployees();
+            switch (operationNumber) {
+                case 1:
+                    company.printEmployees();
+                    break;
 
-            } else if (operationNumber == 2) {
-                company.add();
+                case 2:
+                    company.add();
+                    break;
 
-            } else if (operationNumber == 3) {
-                System.out.print("Podaj numer pracownika do usunięcia: ");
-                int employeeNumber = scanner.nextInt();
-                company.removeEmployee(employeeNumber);
+                case 3:
+                    System.out.print("Podaj numer pracownika do usunięcia: ");
+                    int employeeNumber = scanner.nextInt();
+                    company.removeEmployee(employeeNumber);
+                    break;
 
-            } else if (operationNumber == 9) {
-                break;
+                case EXIT_OPERATION_CODE:
+                    break;
 
-            } else {
-                System.out.println("Operacja o podanym numerze nie istnieje!");
+                default:
+                    System.out.println("Operacja o podanym numerze nie istnieje!");
             }
         }
     }
